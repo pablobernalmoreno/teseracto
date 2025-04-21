a9eebebba95359d615cb52709687e4c08b69a6c2
pablobernalmoreno <pbm1998@hotmail.com>
2025-04-21 14:10:28 -0500

    remove commented code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,14 +22,6 @@ export default function Home() {
       getImageText();
     }
   }, [file]);
-  // (async () => {
-  //   const worker = await createWorker("eng");
-  //   const ret = await worker.recognize(file);
-  //   console.log({ret});
-
-  //   console.log(ret.data.text);
-  //   await worker.terminate();
-  // })();
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
