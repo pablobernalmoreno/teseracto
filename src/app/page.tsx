247751da89e0f4fafa90a7f9c5760bfafcfd887f
pablobernalmoreno <pbm1998@hotmail.com>
2025-04-23 15:49:57 -0500

    handle multiple files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,29 +3,34 @@ import { ChangeEvent, useEffect, useState } from "react";
 import { createWorker, ImageLike } from "tesseract.js";
 
 export default function Home() {
-  const [file, setFile] = useState<ImageLike | File>("");
+  const [files, setFiles] = useState<FileList>();
 
   function handleChange(event: ChangeEvent) {
     const target = event.target as HTMLInputElement;
-    const file: File = (target.files as FileList)[0];
+    const file = (target.files as FileList);
     if (file) {
-      setFile(file);
+      setFiles(file);
     }
   }
 
   const getImageText = async () => {
     const worker = await createWorker("eng");
-    const ret = await worker.recognize(file);
-    const ocrText = ret.data.text;
-    console.log(ocrText);
+    if (files?.length) {
+      for (let i = 0; i < files?.length; ++i) {
+        const file = files[i];
+        const ret = await worker.recognize(file);
+        const ocrText = ret.data.text;
+        console.log(ocrText);
+    }
+    }
     await worker.terminate();
   };
 
   useEffect(() => {
-    if (file !== "") {
+    if (files?.length !== 0) {
       getImageText();
     }
-  }, [file]);
+  }, [files]);
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -35,6 +40,7 @@ export default function Home() {
           <h1>React File Upload</h1>
           <input
             type="file"
+            multiple
             onChange={handleChange}
           />
           <button type="submit">Upload</button>
